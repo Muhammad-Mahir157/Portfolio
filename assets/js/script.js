c3a3438e578f3bbaf66bb98b8b5f990d177cebab
M-Mahir <hmmahir2001@gmail.com>
2024-02-05 16:24:42 +0500

    Completed the initial minimalistic design with all the links incorporated

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+function toggleMenu(){
+    const menue = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menue.classList.toggle("open");
+    icon.classList.toggle("open");
+}
